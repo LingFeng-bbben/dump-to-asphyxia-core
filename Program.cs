8424139f2e99599f2937e8d7e3405913438eb737
LingFeng-bbben <bengy@126.com>
2021-05-29 05:40:01 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace dump_to_asphyxia_core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Asphyxia ID:");
+            string refid = Console.ReadLine();
+            XElement score_dump = XElement.Load("dump.xml");
+            var infos = score_dump.Element("game").Element("music").Elements("info");
+            List<string> scores = new List<string>();
+            foreach (var info in infos)
+            {
+                string[] record = info.Element("param").Value.Split(" ");
+                AsyLine line = new AsyLine();
+                line.Collection = "music";
+                line.Mid = long.Parse(record[0]);
+                line.Type = long.Parse(record[1]);
+                line.Score = long.Parse(record[2]);
+                line.Exscore = 0;
+                line.Clear = long.Parse(record[3]);
+                line.Grade = long.Parse(record[4]);
+                line.ButtonRate = long.Parse(record[7]) / 200;
+                line.LongRate = long.Parse(record[8]) / 200;
+                line.VolRate = long.Parse(record[9]) / 200;
+                line.S = "plugins_profile";
+                line.Refid = refid;
+                line.Id = GetRandomString(16);
+                AtedAt atedAt = new AtedAt();
+                atedAt.Date = System.DateTime.Now.Ticks;
+                line.CreatedAt = atedAt;
+                line.UpdatedAt = atedAt;
+                scores.Add(JsonConvert.SerializeObject(line));
+            }
+            File.WriteAllLines("out.db", scores);
+        }
+        public static string GetRandomString(int length)
+        {
+            byte[] b = new byte[4];
+            new System.Security.Cryptography.RNGCryptoServiceProvider().GetBytes(b);
+            Random r = new Random(BitConverter.ToInt32(b, 0));
+            string s = null, str = "";
+            str += "0123456789"; 
+            str += "abcdefghijklmnopqrstuvwxyz"; 
+            str += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
+            for (int i = 0; i < length; i++)
+            {
+                s += str.Substring(r.Next(0, str.Length - 1), 1);
+            }
+            return s;
+        }
+    }
+}
---
+++ asphyxia_line.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dump_to_asphyxia_core
+{
+    public partial class AsyLine
+    {
+        [JsonProperty("collection")]
+        public string Collection { get; set; }
+
+        [JsonProperty("mid")]
+        public long Mid { get; set; }
+
+        [JsonProperty("type")]
+        public long Type { get; set; }
+
+        [JsonProperty("score")]
+        public long Score { get; set; }
+
+        [JsonProperty("exscore")]
+        public long Exscore { get; set; }
+
+        [JsonProperty("clear")]
+        public long Clear { get; set; }
+
+        [JsonProperty("grade")]
+        public long Grade { get; set; }
+
+        [JsonProperty("buttonRate")]
+        public long ButtonRate { get; set; }
+
+        [JsonProperty("longRate")]
+        public long LongRate { get; set; }
+
+        [JsonProperty("volRate")]
+        public long VolRate { get; set; }
+
+        [JsonProperty("__s")]
+        public string S { get; set; }
+
+        [JsonProperty("__refid")]
+        public string Refid { get; set; }
+
+        [JsonProperty("_id")]
+        public string Id { get; set; }
+
+        [JsonProperty("createdAt")]
+        public AtedAt CreatedAt { get; set; }
+
+        [JsonProperty("updatedAt")]
+        public AtedAt UpdatedAt { get; set; }
+    }
+
+    public partial class AtedAt
+    {
+        [JsonProperty("$$date")]
+        public long Date { get; set; }
+    }
+}
